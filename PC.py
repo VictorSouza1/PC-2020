28f530955b1b2ca4cec088ef268d7400899fb378
Victor Souza <victorsm@id.uff.br>
2020-11-30 12:42:34 -0300

    versao nova ainda com round (precisa remover o round ainda )

--- PC.py
+++ PC.py
@@ -1,18 +1,34 @@
 import pprint
 
-A = [[0,0,10,5,6,7],
-	[0,20,20,21,14,4],
-	[90,65,82,64,10,3],
-	[90,101,12,49,14,7],
-	[90,29,46,48,20,7],
-	[90,101,92,83,20,4],
-	[90,65,80,84,15,5],
-	[90,92,82,53,11,2],
-	[90,101,79,47,5,9],
-	[90,47,19,28,20,0],
-	[90,20,35,86,0,0],
-	[90,92,30,0,0,0]]
+A2 = [[10,5,6,7,0,0,0,0,0,0,0,0],
+	[20,20,21,14,4,0,0,0,0,0,0,0],
+	[90,65,82,64,10,3,0,0,0,0,0,0],
+	[0,90,101,12,49,14,7,0,0,0,0,0],
+	[0,0,90,29,46,48,20,7,0,0,0,0],
+	[0,0,0,90,101,92,83,20,4,0,0,0],
+	[0,0,0,0,90,65,80,84,15,5,0,0],
+	[0,0,0,0,0,90,92,82,53,11,2,0],
+	[0,0,0,0,0,0,90,101,79,47,5,9],
+	[0,0,0,0,0,0,0,90,47,19,28,20],
+	[0,0,0,0,0,0,0,0,90,20,35,86],
+	[0,0,0,0,0,0,0,0,0,90,92,30]]
 
+B = [28,79,314,273,240,390,339,330,331,204,231,212]
+
+
+A = [[0,0,10,5,6,7,0,0],
+	[0,20,20,21,14,4,0,0],
+	[90,65,82,64,10,3,0,0],
+	[90,101,12,49,14,7,0,0],
+	[90,29,46,48,20,7,0,0],
+	[90,101,92,83,20,4,0,0],
+	[90,65,80,84,15,5,0,0],
+	[90,92,82,53,11,2,0,0],
+	[90,101,79,47,5,9,0,0],
+	[90,47,19,28,20,0,0,0],
+	[90,20,35,86,0,0,0,0],
+	[90,92,30,0,0,0,0,0]]
+    
 li = 2
 ls = 3
 
@@ -41,21 +57,42 @@ def TorcarLinhas (M, l1, l2):
     M[l2] = temp
     return 
 
-def parserAcesso (A, m, n):
+def parseTrocarLinhas (l1, l2):
+    global A
+    for i in range (len(A)):
+        temp = parserAcesso(l1, i)
+        parserEscrita(l1, i, parserAcesso(l2,i))
+        parserEscrita(l2, i, temp)
+        
+
+def parserAcesso (m, n):
     global li
-    return A[m][n - m + li + 1] 
+    global A
+    if (n - m + li) < 0:
+        return 0
+    if (n - m + li) >= len(A[0]):
+        return 0
+    return A[m][n - m + li] 
     
 
 
-def parserEscrita (m, n):
-    pass
+def parserEscrita (m, n, valor):
+    global li
+    global A
+    if (n - m + li) < 0:
+        return
+    if (n - m + li) >= len(A[0]):
+        return
+    A[m][n - m + li] = valor
+
+    return
 
 def LU (A,B):
     pp = pprint.PrettyPrinter()
     L = InicializaMatriz(len(A))
 
 
-    pp.pprint(A)
+    #pp.pprint(A)
 
 
     for c in range (len(A)):
@@ -67,19 +104,27 @@ def LU (A,B):
                 Lmax = l+c
                 max = A[l+c][c]
 
+        print ('matiz A antes da troca')
+        pp.pprint(A)
         print('pivo = ', max, 'linha do pivo = ', Lmax)        
         TorcarLinhas(A, Lmax, c)                    # Trocar linha do pivô para posiciona-la corretamente
         TorcarLinhas(B, Lmax, c)                    # Realizar mesma troca no matriz B  
         TorcarLinhas(L, Lmax, c)                    # Realizar mesma troca no matriz L              
+        print ('matiz A depois da troca')
         pp.pprint(A)
 
         for i in range (len(A)-c-1):            #  Fatorçao LU
-            coef = A[i+1+c][c] / A[c][c]
-            L[i+1+c][c] = coef                  # adicionando os elementos da matriz L (matriz de coeficientes) ja na posição correta
-            print('coeficiente = ', A[i+1+c][c], '/', A[c][c], '=', coef)
+            coef = round(A[i+1+c][c] / A[c][c], 2)
+            L[i+1+c][c] = coef  
+            print ("Adicionando o coeficiente",coef, " na posicao ", i+1+c, ",", c)
+            print("cada passo de L : ")
+            pp.pprint(L)
+                            # adicionando os elementos da matriz L (matriz de coeficientes) ja na posição correta
+            #print('coeficiente = ', A[i+1+c][c], '/', A[c][c], '=', coef)
+            
             for j in range (len(A)):            # zerando/operando sobre uma linha
-                print('calculando : ', A[i+1+c][j], '- (', coef, ') * ', A[c][j], ' = ', A[i+1+c][j] - coef * A[c][j])
-                A[i+1+c][j] = A[i+1+c][j] - coef * A[c][j]
+                #print('calculando : ', A[i+1+c][j], '- (', coef, ') * ', A[c][j], ' = ', A[i+1+c][j] - coef * A[c][j])
+                A[i+1+c][j] = round(A[i+1+c][j] - coef * A[c][j], 2)
 
         
  
@@ -94,7 +139,6 @@ def LU (A,B):
     pp.pprint(B)
     
     return
-        
 
 def LUnova ():
     global A
@@ -104,34 +148,40 @@ def LUnova ():
     pp = pprint.PrettyPrinter()
     L = InicializaMatriz(len(A))
 
-    for j in range(len(A[0])):
-        print (A[0][j-0+li])
-    #pp.pprint(A)
-
-    '''
     for c in range (len(A)):
         
-        max = A[c][c]                           #pivô
+        max = parserAcesso(c , c)                          #pivô
         Lmax = c
+        
         for l in range (len(A)-c):              # Achar o menor elemento que pode ser um pivô
-            if abs(A[l+c][c]) > max:
+            if abs(parserAcesso(l+c, c)) > max:
                 Lmax = l+c
-                max = A[l+c][c]
+                max = parserAcesso(l+c,c)
 
-        print('pivo = ', max, 'linha do pivo = ', Lmax)        
-        TorcarLinhas(A, Lmax, c)                    # Trocar linha do pivô para posiciona-la corretamente
-        TorcarLinhas(B, Lmax, c)                    # Realizar mesma troca no matriz B  
+        #print('pivo = ', max, 'linha do pivo = ', Lmax)        
+        
+        parseTrocarLinhas(Lmax, c)                    # Trocar linha do pivô para posiciona-la corretamente
+        #print ('primeiro A ficou ')
+        #pp.pprint(A)
+        #TorcarLinhas(B, Lmax, c)                    # Realizar mesma troca no matriz B  
+        
         TorcarLinhas(L, Lmax, c)                    # Realizar mesma troca no matriz L              
-        pp.pprint(A)
-
+     
+        #pp.pprint(A)
+        
+        
         for i in range (len(A)-c-1):            #  Fatorçao LU
-            coef = A[i+1+c][c] / A[c][c]
+            coef = round(parserAcesso(i+1+c, c) / parserAcesso(c, c), 2)
             L[i+1+c][c] = coef                  # adicionando os elementos da matriz L (matriz de coeficientes) ja na posição correta
-            print('coeficiente = ', A[i+1+c][c], '/', A[c][c], '=', coef)
+            print ("Adicionando o coeficiente na posicao ", i+1+c, ",", c)
+            print("cada passo de L : ")
+            pp.pprint(L)
+            #print('coeficiente = ', parserAcesso(i+1+c, c), '/', parserAcesso(c, c), '=', coef)
+            
             for j in range (len(A)):            # zerando/operando sobre uma linha
-                print('calculando : ', A[i+1+c][j], '- (', coef, ') * ', A[c][j], ' = ', A[i+1+c][j] - coef * A[c][j])
-                A[i+1+c][j] = A[i+1+c][j] - coef * A[c][j]
-
+                #print('calculando : ', parserAcesso(i+1+c, j) , '- (', coef, ') * ',parserAcesso(c,j) , ' = ', parserAcesso(i+1+c, j) - coef * parserAcesso(c,j))
+                parserEscrita(i+1+c,j, round((parserAcesso(i+1+c, j) - coef * parserAcesso(c,j)),2)) 
+            
         
  
 
@@ -141,9 +191,9 @@ def LUnova ():
     pp.pprint(A)
     print('matriz l ')
     pp.pprint(L)
-    print('matriz b ')
-    pp.pprint(B)
-    '''
+    #print('matriz b ')
+    #pp.pprint(B)
+
     return
 
 
@@ -155,3 +205,5 @@ if __name__ == "__main__":
     #B = [11, -15, 29]
 
     LUnova ()
+
+    #LU(A,B)
